01fb4f6843b568e8ba5bd04c047b99d83b70c3fc
Olivia Persson <olivia.persson@forefront.se>
2023-01-09 15:23:35 +0100

    Add background styling

---
+++ src/components/StartPage.jsx
@@ -0,0 +1,13 @@
+import "./StartPage.css";
+
+function StartPage() {
+  return (
+    <div className="start-page-container">
+      <h1>Quizzical</h1>
+      <p>Some description if needed</p>
+      <button>Start quiz</button>
+    </div>
+  );
+}
+
+export default StartPage;
