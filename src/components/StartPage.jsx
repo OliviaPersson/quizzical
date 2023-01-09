ebb33bd5e00eb9757b19941cae3e8d159dfd2ee2
Olivia Persson <olivia.persson@forefront.se>
2023-01-09 16:00:29 +0100

    Add content on start page

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -3,9 +3,11 @@ import "./StartPage.css";
 function StartPage() {
   return (
     <div className="start-page-container">
-      <h1>Quizzical</h1>
-      <p>Some description if needed</p>
-      <button>Start quiz</button>
+      <h1 className="header">Quizzical</h1>
+      <p className="description">Some description if needed</p>
+      <div className="button-container">
+        <button className="start-button">Start quiz</button>
+      </div>
     </div>
   );
 }
