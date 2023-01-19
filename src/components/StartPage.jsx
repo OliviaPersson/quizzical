6e924a71b7b71c758b2f3fa24fe5ed559348f15d
Olivia Persson <olivia.persson@forefront.se>
2023-01-19 14:18:45 +0100

    Add input field on startpage

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -4,12 +4,21 @@ function StartPage({ handleStartQuizz }) {
   return (
     <div className="start-page-container">
       <h1 className="header">Quizzical</h1>
-      <p className="description">Some description if needed</p>
-      <div className="button-container">
-        <button onClick={handleStartQuizz} className="start-button">
-          Start quiz
-        </button>
-      </div>
+      <p className="form-label">What should we call you?</p>
+      <form className="quiz-form">
+        <input
+          className="form-input"
+          placeholder="Name:"
+          type="text"
+          name="name"
+        />
+        {/* <input type="submit" value="Submit" /> */}
+        <div className="button-container">
+          <button onClick={handleStartQuizz} className="start-button">
+            Start quiz
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
